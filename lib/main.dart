86a1e697cad814f2a388e4e407ff47656cfa7642
Syed Ashiq <imsyedashiq@gmail.com>
2022-01-02 20:01:23 +0600

    last

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,25 @@
 import 'package:flutter/material.dart';
+import 'package:audioplayers/audioplayers.dart';
 
-void main() {
-  runApp(MaterialApp(
-    home: Scaffold(
-      appBar: AppBar(
-        title: Text('Ashiq'),
+void main() => runApp(xyloPhoneApp());
+
+class xyloPhoneApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        body: SafeArea(
+          child: Center(
+            child: TextButton(
+              onPressed: () {
+                final Player = AudioCache();
+                Player.play('note1.wav');
+              },
+              child: Text('Click Me!'),
+            ),
+          ),
+        ),
       ),
-    ),
-  ));
+    );
+  }
 }
