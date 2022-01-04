08191c9b2035c4f98382b590063e5138685d9d9a
Syed Ashiq <imsyedashiq@gmail.com>
2022-01-04 07:51:06 +0600

    7 color added with sound

--- lib/main.dart
+++ lib/main.dart
@@ -9,14 +9,87 @@ class xyloPhoneApp extends StatelessWidget {
     return MaterialApp(
       home: Scaffold(
         body: SafeArea(
-          child: Center(
-            child: TextButton(
-                onPressed: (){
-                  final Player = AudioCache();
-                  Player.play('note5.wav');
-                },
-                child: Text('Play Now !'),
-          ),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              Expanded(
+                child: InkWell(
+                  child: Container(
+                    color: Colors.red,
+                  ),
+                  onTap: () {
+                    final Player = AudioCache();
+                    Player.play('note1.wav');
+                  },
+                ),
+              ),
+              Expanded(
+                child: InkWell(
+                  child: Container(
+                    color: Colors.green,
+                  ),
+                  onTap: () {
+                    final Player = AudioCache();
+                    Player.play('note2.wav');
+                  },
+                ),
+              ),
+              Expanded(
+                child: InkWell(
+                  child: Container(
+                    color: Colors.blue,
+                  ),
+                  onTap: () {
+                    final Player = AudioCache();
+                    Player.play('note3.wav');
+                  },
+                ),
+              ),
+              Expanded(
+                child: InkWell(
+                  child: Container(
+                    color: Colors.teal,
+                  ),
+                  onTap: () {
+                    final Player = AudioCache();
+                    Player.play('note4.wav');
+                  },
+                ),
+              ),
+              Expanded(
+                child: InkWell(
+                  child: Container(
+                    color: Colors.purple,
+                  ),
+                  onTap: () {
+                    final Player = AudioCache();
+                    Player.play('note5.wav');
+                  },
+                ),
+              ),
+              Expanded(
+                child: InkWell(
+                  child: Container(
+                    color: Colors.cyan,
+                  ),
+                  onTap: () {
+                    final Player = AudioCache();
+                    Player.play('note6.wav');
+                  },
+                ),
+              ),
+              Expanded(
+                child: InkWell(
+                  child: Container(
+                    color: Colors.orange,
+                  ),
+                  onTap: () {
+                    final Player = AudioCache();
+                    Player.play('note7.wav');
+                  },
+                ),
+              ),
+            ],
           ),
         ),
       ),
